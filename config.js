106d9054000fb1b2fec28bd254c0155cc768c331
duong23407 <buitruongduong2342000@gmail.com>
2024-12-11 21:24:48 +0700

    guinguoithuong

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "dương dành tặng người mình thương",
+    introTitle: 'ai đó à',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '💗 Click 💗',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu lắm 💝',
+    btnNo: 'Không nha 😂',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Thảo lại yêu mình 🤣',
+    btnReply: '💖 Gửi cho Dương 💖',
+    reply: 'Yêu thì yêu mà không yêu thì yêu 😋',
+    mess: 'Đậu biết mà 🥰. Yêu Thảo nhiều nhiều 😘😘',
+    messDesc: 'Anh hứa , sau này có tiền anh sẽ nuôi em thành HEO.',
+    btnAccept: 'Okiiiii lun 🥰',
+    messLink: 'https://github.com/DauDau432/Confess-Crush' //link mess của các bạn.
+}
